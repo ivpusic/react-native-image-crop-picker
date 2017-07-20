63e6f71db1869129add93a4f987bd415571805ed
deathemperor <deathemperor@gmail.com>
2017-07-20 11:41:49 +0700

    fixed a bug when using openCamera with [multiple: true]
    related issue: https://github.com/ivpusic/react-native-image-crop-picker/issues/353

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
@@ -644,6 +644,7 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
                 startCropping(activity, uri);
             } else {
                 try {
+                    resultCollector.setWaitCount(1);
                     resultCollector.notifySuccess(getSelection(activity, uri, true));
                 } catch (Exception ex) {
                     resultCollector.notifyProblem(E_NO_IMAGE_DATA_FOUND, ex.getMessage());
@@ -657,6 +658,7 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
             final Uri resultUri = UCrop.getOutput(data);
             if (resultUri != null) {
                 try {
+                    resultCollector.setWaitCount(1);
                     resultCollector.notifySuccess(getSelection(activity, resultUri, false));
                 } catch (Exception ex) {
                     resultCollector.notifyProblem(E_NO_IMAGE_DATA_FOUND, ex.getMessage());
