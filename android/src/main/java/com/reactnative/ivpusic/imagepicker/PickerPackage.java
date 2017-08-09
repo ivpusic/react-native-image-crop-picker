6f24955b804cc9aaf1de84d3023c974aef3b15fb
Ivan Pusic <pusic007@gmail.com>
2017-08-09 13:55:55 +0200

    0.47 backward compatibile android support

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerPackage.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerPackage.java
@@ -15,6 +15,7 @@ import java.util.List;
  */
 public class PickerPackage implements ReactPackage {
 
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
