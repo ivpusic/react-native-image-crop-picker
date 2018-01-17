6bbfc643eddcbc9195146dff682363ec1d0f4b48
Jason Gaare <jason.d.gaare@gmail.com>
2018-01-17 10:35:47 -0600

    fix typo in creationDate property in response (#573)

--- ios/ImageCropPicker.m
+++ ios/ImageCropPicker.m
@@ -439,7 +439,7 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
              @"data": (data) ? data : [NSNull null],
              @"exif": (exif) ? exif : [NSNull null],
              @"cropRect": CGRectIsNull(cropRect) ? [NSNull null] : [ImageCropPicker cgRectToDictionary:cropRect],
-             @"creationDate:": (creationDate) ? [NSString stringWithFormat:@"%.0f", [creationDate timeIntervalSince1970]] : [NSNull null],
+             @"creationDate": (creationDate) ? [NSString stringWithFormat:@"%.0f", [creationDate timeIntervalSince1970]] : [NSNull null],
              @"modificationDate": (modificationDate) ? [NSString stringWithFormat:@"%.0f", [modificationDate timeIntervalSince1970]] : [NSNull null],
              };
 }
