fbe3727d70835c5002b8855d084bab01d1b10841
kentos <kent@kentcederstrom.se>
2018-04-05 09:23:53 +0200

    Adding missing import

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/RealPathUtil.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/RealPathUtil.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.webkit.MimeTypeMap;
 
 import java.io.File;
