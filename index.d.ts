e7ad393b7ef7df8e249d87c072b5ec42ada225c8
Martin Richter <me@martinrichter.net>
2018-04-23 15:20:27 +0200

    Update TypeScript typings with some Android-only options
    Several Android-only options were missing from the TypeScript `Options` interface, causing compiler warnings in client codebases when used.

--- index.d.ts
+++ index.d.ts
@@ -7,8 +7,13 @@ declare module "react-native-image-crop-picker" {
         path?: string;
         includeBase64?: boolean;
         includeExif?: boolean;
+        cropperActiveWidgetColor?: string;
+        cropperStatusBarColor?: string;
+        cropperToolbarColor?: string;
+        freeStyleCropEnabled?: boolean;
         cropperTintColor?: string;
         cropperCircleOverlay?: boolean;
+        disableCropperColorSetters?: boolean;
         maxFiles?: number;
         waitAnimationEnd?: boolean;
         smartAlbums?: string[];
