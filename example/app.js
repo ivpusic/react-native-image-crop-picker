fab0164e1b34f4f134cbaaaba6d5cbe34e38032c
Miguel <miguelsaizo@gmail.com>
2018-08-30 17:50:20 -0400

    Fix typo on example (#809)

--- example/app.js
+++ example/app.js
@@ -134,7 +134,7 @@ export default class App extends Component {
       multiple: true,
       waitAnimationEnd: false,
       includeExif: true,
-      forgeJpg: true,
+      forceJpg: true,
     }).then(images => {
       this.setState({
         image: null,
