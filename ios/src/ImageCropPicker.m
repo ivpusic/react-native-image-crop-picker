f71092039e74899c77499d511ec8d5a245327cd8
Michael Lefkowitz <1746853+lfkwtz@users.noreply.github.com>
2018-12-07 14:26:26 -0600

    revert #834 (ios changes only)

--- ios/src/ImageCropPicker.m
+++ ios/src/ImageCropPicker.m
@@ -839,7 +839,11 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
                    didCropImage:(UIImage *)croppedImage
                   usingCropRect:(CGRect)cropRect {
 
-    ImageResult *imageResult = [self.compression compressImage:croppedImage withOptions:self.options];
+    // we have correct rect, but not correct dimensions
+    // so resize image
+    CGSize resizedImageSize = CGSizeMake([[[self options] objectForKey:@"width"] intValue], [[[self options] objectForKey:@"height"] intValue]);
+    UIImage *resizedImage = [croppedImage resizedImageToFitInSize:resizedImageSize scaleIfSmaller:YES];
+    ImageResult *imageResult = [self.compression compressImage:resizedImage withOptions:self.options];
 
     NSString *filePath = [self persistFile:imageResult.data];
     if (filePath == nil) {
