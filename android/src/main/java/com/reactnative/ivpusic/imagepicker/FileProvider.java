8c8674bdc27614c6cafc6c6b7fbedfa86eabac19
Harkirat <saluja.harkirat@gmail.com>
2019-07-10 15:34:52 +0530

    fixing multiple file provider issue

---
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/FileProvider.java
@@ -0,0 +1,10 @@
+package com.reactnative.ivpusic.imagepicker;
+
+/**
+ * Providing a custom <code>FileProvider</code> prevents manifest <code>&lt;provider&gt;</code> name collisions.
+ */
+public class FileProvider extends android.support.v4.content.FileProvider {
+
+    // This class intentionally left blank.
+
+}
