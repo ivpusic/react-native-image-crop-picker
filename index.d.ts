5ffa3ff10d6c22aab493348871f42c477a4b893d
Dacri Burdan <dburdan@gmail.com>
2020-07-04 13:46:35 -0700

    Added picker error code types

--- index.d.ts
+++ index.d.ts
@@ -60,6 +60,29 @@ declare module "react-native-image-crop-picker" {
         height: number;
     }
 
+    type PickerErrorCodeCommon =
+        | 'E_PICKER_CANCELLED'
+        | 'E_NO_IMAGE_DATA_FOUND'
+        | 'E_PERMISSION_MISSING'
+        | 'E_ERROR_WHILE_CLEANING_FILES';
+
+    type PickerErrorCodeIOS =
+        | 'E_PICKER_CANNOT_RUN_CAMERA_ON_SIMULATOR'
+        | 'E_PICKER_NO_CAMERA_PERMISSION'
+        | 'E_CROPPER_IMAGE_NOT_FOUND'
+        | 'E_CANNOT_SAVE_IMAGE'
+        | 'E_CANNOT_PROCESS_VIDEO';
+
+    type PickerErrorCodeAndroid =
+        | 'E_ACTIVITY_DOES_NOT_EXIST'
+        | 'E_CALLBACK_ERROR'
+        | 'E_FAILED_TO_SHOW_PICKER'
+        | 'E_FAILED_TO_OPEN_CAMERA'
+        | 'E_CAMERA_IS_NOT_AVAILABLE'
+        | 'E_CANNOT_LAUNCH_CAMERA';
+
+    export type PickerErrorCode = PickerErrorCodeCommon | PickerErrorCodeIOS | PickerErrorCodeAndroid;
+
     export function openPicker(options: Options): Promise<Image | Image[]>;
     export function openCamera(options: Options): Promise<Image | Image[]>;
     export function openCropper(options: Options): Promise<Image>;
