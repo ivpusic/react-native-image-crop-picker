c887d7f2d729e5ef27eaa22993f56846d8ad0d57
vinit_groww <vinitraj1012@gmail.com>
2020-08-10 22:16:55 +0530

    context passed exception handled

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/Compression.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/Compression.java
@@ -7,8 +7,10 @@ import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReadableMap;
 
 import java.io.BufferedOutputStream;
@@ -26,53 +28,57 @@ import java.util.UUID;
 
 class Compression {
 
-    File resize(String originalImagePath, int maxWidth, int maxHeight, int quality) throws IOException {
-        Bitmap original = BitmapFactory.decodeFile(originalImagePath);
+    File resize(String originalImagePath, int maxWidth, int maxHeight, int quality, ReactApplicationContext reactApplicationContext) throws IOException {
+    	File resizeImageFile = null;
+    	try {
+		    Bitmap original = BitmapFactory.decodeFile(originalImagePath);
+		    int width = original.getWidth();
+		    int height = original.getHeight();
 
-        int width = original.getWidth();
-        int height = original.getHeight();
+		    // Use original image exif orientation data to preserve image orientation for the resized bitmap
+		    ExifInterface originalExif = new ExifInterface(originalImagePath);
+		    int originalOrientation = originalExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
 
-        // Use original image exif orientation data to preserve image orientation for the resized bitmap
-        ExifInterface originalExif = new ExifInterface(originalImagePath);
-        int originalOrientation = originalExif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
+		    Matrix rotationMatrix = new Matrix();
+		    int rotationAngleInDegrees = getRotationInDegreesForOrientationTag(originalOrientation);
+		    rotationMatrix.postRotate(rotationAngleInDegrees);
 
-        Matrix rotationMatrix = new Matrix();
-        int rotationAngleInDegrees = getRotationInDegreesForOrientationTag(originalOrientation);
-        rotationMatrix.postRotate(rotationAngleInDegrees);
+		    float ratioBitmap = (float) width / (float) height;
+		    float ratioMax = (float) maxWidth / (float) maxHeight;
 
-        float ratioBitmap = (float) width / (float) height;
-        float ratioMax = (float) maxWidth / (float) maxHeight;
+		    int finalWidth = maxWidth;
+		    int finalHeight = maxHeight;
 
-        int finalWidth = maxWidth;
-        int finalHeight = maxHeight;
+		    if (ratioMax > 1) {
+			    finalWidth = (int) ((float) maxHeight * ratioBitmap);
+		    } else {
+			    finalHeight = (int) ((float) maxWidth / ratioBitmap);
+		    }
 
-        if (ratioMax > 1) {
-            finalWidth = (int) ((float) maxHeight * ratioBitmap);
-        } else {
-            finalHeight = (int) ((float) maxWidth / ratioBitmap);
-        }
+		    Bitmap resized = Bitmap.createScaledBitmap(original, finalWidth, finalHeight, true);
+		    resized = Bitmap.createBitmap(resized, 0, 0, finalWidth, finalHeight, rotationMatrix, true);
 
-        Bitmap resized = Bitmap.createScaledBitmap(original, finalWidth, finalHeight, true);
-        resized = Bitmap.createBitmap(resized, 0, 0, finalWidth, finalHeight, rotationMatrix, true);
-        
-        File imageDirectory = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
+		    File imageDirectory = Environment.getExternalStoragePublicDirectory(
+				    Environment.DIRECTORY_PICTURES);
 
-        if(!imageDirectory.exists()) {
-            Log.d("image-crop-picker", "Pictures Directory is not existing. Will create this directory.");
-            imageDirectory.mkdirs();
-        }
+		    if(!imageDirectory.exists()) {
+			    Log.d("image-crop-picker", "Pictures Directory is not existing. Will create this directory.");
+			    imageDirectory.mkdirs();
+		    }
 
-        File resizeImageFile = new File(imageDirectory, UUID.randomUUID() + ".jpg");
+		    resizeImageFile = new File(imageDirectory, UUID.randomUUID() + ".jpg");
 
-        OutputStream os = new BufferedOutputStream(new FileOutputStream(resizeImageFile));
-        resized.compress(Bitmap.CompressFormat.JPEG, quality, os);
+		    OutputStream os = new BufferedOutputStream(new FileOutputStream(resizeImageFile));
+		    resized.compress(Bitmap.CompressFormat.JPEG, quality, os);
 
-        os.close();
-        original.recycle();
-        resized.recycle();
-
-        return resizeImageFile;
+		    os.close();
+		    original.recycle();
+		    resized.recycle();
+	    } catch (Exception err) {
+    		err.printStackTrace();
+		    Toast.makeText(reactApplicationContext,"Something went wrong",Toast.LENGTH_LONG);
+	    }
+    	return resizeImageFile;
     }
 
     int getRotationInDegreesForOrientationTag(int orientationTag) {
@@ -88,7 +94,7 @@ class Compression {
         }
     }
 
-    File compressImage(final ReadableMap options, final String originalImagePath, final BitmapFactory.Options bitmapOptions) throws IOException {
+    File compressImage(final ReadableMap options, final String originalImagePath, final BitmapFactory.Options bitmapOptions, final ReactApplicationContext reactContext) throws IOException {
         Integer maxWidth = options.hasKey("compressImageMaxWidth") ? options.getInt("compressImageMaxWidth") : null;
         Integer maxHeight = options.hasKey("compressImageMaxHeight") ? options.getInt("compressImageMaxHeight") : null;
         Double quality = options.hasKey("compressImageQuality") ? options.getDouble("compressImageQuality") : null;
@@ -123,7 +129,7 @@ class Compression {
             maxHeight = Math.min(maxHeight, bitmapOptions.outHeight);
         }
 
-        return resize(originalImagePath, maxWidth, maxHeight, targetQuality);
+        return resize(originalImagePath, maxWidth, maxHeight, targetQuality, reactContext);
     }
 
     synchronized void compressVideo(final Activity activity, final ReadableMap options, final String originalVideo, final String compressedVideo, final Promise promise) {
--- android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
@@ -574,7 +574,7 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
 
         // if compression options are provided image will be compressed. If none options is provided,
         // then original image will be returned
-        File compressedImage = compression.compressImage(options, path, original);
+        File compressedImage = compression.compressImage(options, path, original,this.reactContext);
         String compressedImagePath = compressedImage.getPath();
         BitmapFactory.Options options = validateImage(compressedImagePath);
         long modificationDate = new File(path).lastModified();
@@ -736,7 +736,7 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
             if (resultUri != null) {
                 try {
                     if (width > 0 && height > 0) {
-                        resultUri = Uri.fromFile(compression.resize(resultUri.getPath(), width, height, 100));
+                        resultUri = Uri.fromFile(compression.resize(resultUri.getPath(), width, height, 100, this.reactContext));
                     }
 
                     WritableMap result = getSelection(activity, resultUri, false);
@@ -776,8 +776,7 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
     }
 
     private boolean isCameraAvailable(Activity activity) {
-        return activity.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)
-                || activity.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+        return activity.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
     }
 
     private File createImageFile() throws IOException {
