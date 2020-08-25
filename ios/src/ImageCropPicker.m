6618d321b5c1db6a66186025594ecd573676e9d2
Gibbs <webh4ck@naver.com>
2020-08-25 20:38:20 +0900

    fix(ios): add extension determined by a first byte

--- ios/src/ImageCropPicker.m
+++ ios/src/ImageCropPicker.m
@@ -518,6 +518,24 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
     return @"";
 }
 
+- (NSString *)determineExtensionFromImageData:(NSData *)data {
+    uint8_t c;
+    [data getBytes:&c length:1];
+
+    switch (c) {
+        case 0xFF:
+            return @".jpg";
+        case 0x89:
+            return @".png";
+        case 0x47:
+            return @".gif";
+        case 0x49:
+        case 0x4D:
+            return @".tiff";
+    }
+    return @".jpg";
+}
+
 - (void)qb_imagePickerController:
 (QBImagePickerController *)imagePickerController
           didFinishPickingAssets:(NSArray *)assets {
@@ -843,16 +861,8 @@ RCT_EXPORT_METHOD(openCropper:(NSDictionary *)options
     // create temp file
     NSString *tmpDirFullPath = [self getTmpDirectory];
     NSString *filePath = [tmpDirFullPath stringByAppendingString:[[NSUUID UUID] UUIDString]];
-    NSString *mimeType = [self determineMimeTypeFromImageData:data];
-    if([mimeType isEqualToString:@"image/jpeg"]){
-     filePath = [filePath stringByAppendingString:@".jpg"];
-    }
-    else if([mimeType isEqualToString:@"image/gif"]){
-     filePath = [filePath stringByAppendingString:@".gif"];
-    }
-    else {
-        filePath = [filePath stringByAppendingString:@".jpg"];
-    }
+    NSString *extension = [self determineExtensionFromImageData:data];
+    filePath = [filePath stringByAppendingString:extension];
     // save cropped file
     BOOL status = [data writeToFile:filePath atomically:YES];
     if (!status) {
