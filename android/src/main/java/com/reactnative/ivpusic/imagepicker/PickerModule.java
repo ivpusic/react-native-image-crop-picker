32575330a29cdf45063b7cce81f32ae4965cb887
Ruslan Baybekov <39907210+oxyii@users.noreply.github.com>
2020-10-01 12:57:57 +0300

    [android] filter image types if cropping only (#1422)
    ref #1421

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
@@ -342,8 +342,10 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
 
             if (cropping || mediaType.equals("photo")) {
                 galleryIntent.setType("image/*");
-                String[] mimetypes = {"image/jpeg", "image/png"};
-                galleryIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
+                if (cropping) {
+                    String[] mimetypes = {"image/jpeg", "image/png"};
+                    galleryIntent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
+                }
             } else if (mediaType.equals("video")) {
                 galleryIntent.setType("video/*");
             } else {
