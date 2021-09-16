808c7252de580b52d6e14cc0d9d6525aaa7c1b92
David Kuo <me@davy.tw>
2021-09-16 02:40:04 +0000

    fix(export): export named functions to match definitions

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import React from 'react';
 
 import {NativeModules} from 'react-native';
-export default NativeModules.ImageCropPicker;
+
+const ImageCropPicker = NativeModules.ImageCropPicker;
+
+export default ImageCropPicker;
+export const openPicker = ImageCropPicker.openPicker;
+export const openCamera = ImageCropPicker.openCamera;
+export const openCropper = ImageCropPicker.openCropper;
+export const clean = ImageCropPicker.clean;
+export const cleanSingle = ImageCropPicker.cleanSingle;
