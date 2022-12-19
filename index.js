4c5e30122ab2b29c301a3bcb9832917beae87cf8
Pavel Chuchuva <pavel@chuchuva.com>
2022-12-20 10:00:06 +1100

    (ios) Add geolocation data to Exif
    Copy Latitude and Longitude to `exif` property similar to Android.
    
    fixes #751

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ import {NativeModules} from 'react-native';
 
 const ImageCropPicker = NativeModules.ImageCropPicker;
 
+const nativeOpenPicker = ImageCropPicker.openPicker;
+ImageCropPicker.openPicker = options => {
+  return nativeOpenPicker(options).then(res => {
+    (Array.isArray(res) ? res : [res]).forEach(image => {
+      const gps = image?.exif?.["{GPS}"];
+      if (gps?.Latitude && gps?.Longitude) {
+        image.exif.Latitude = gps.LatitudeRef === 'S' ? -gps.Latitude : gps.Latitude;
+        image.exif.Longitude = gps.LongitudeRef === 'W' ? -gps.Longitude : gps.Longitude;
+      }
+    });
+    return res;
+  });
+}
+
 export default ImageCropPicker;
 export const openPicker = ImageCropPicker.openPicker;
 export const openCamera = ImageCropPicker.openCamera;
